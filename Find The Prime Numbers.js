78010289a3880f25f2f537db268c076f858ea4e5
mahmoudKSanad <sanadmahmoud50@gmail.com>
2024-12-01 21:19:13 +0200

    Add files via upload

---
+++ Find The Prime Numbers.js
@@ -0,0 +1,17 @@
+function primeNumbers(n, m) {
+  let list = [];
+  for (num = n; num <= m; num++) {
+    let isPrime = true;
+    for (i = 2; i <= num - 1; i++) {
+      if (Number.isInteger(num / i)) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime && num >= 2) {
+      list.push(num);
+    }
+  }
+  return list.join(" ");
+}
+console.log(primeNumbers(-1, 70));
